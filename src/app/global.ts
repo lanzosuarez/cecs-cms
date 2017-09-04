6de51a04d72de8633af5234bdda4de730ce00065
Pons <lanzosuarez@gmail.com>
2017-09-04 15:22:03 +0800

    change global variable

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,5 @@
 export var url: string = 'http://localhost:3000';
 
 export const setUrl = () => {
-    url = 'https://av-autovault-api.herokuapp.com';
+    url = 'https://cinfodroid-api.herokuapp.com';
 }
