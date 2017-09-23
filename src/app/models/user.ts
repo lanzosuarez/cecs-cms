7189b44d4c3319da930cdde12ee9952000d3dc54
Pons <lanzosuarez@gmail.com>
2017-09-23 23:59:11 +0800

    api

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,6 +2,8 @@ export class User {
     constructor(
         public username: string,
         public name: string,
-        public email: string
+        public email: string,
+        public firstname?: string,
+        public lastname?: string,
     ) { }
 }
